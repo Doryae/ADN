0bab1182100615b3c20c403a5f1528b2207fb083
Taverniers Dylan <89921675+Doryae@users.noreply.github.com>
2025-11-24 14:20:17 +0100

    [Feat] Added a new DB and a new Button referencing said DB
    + Updated the two DB to have more contents

--- gen.js
+++ gen.js
@@ -6,7 +6,17 @@ fetch("./idea.json")
         const db = data
         console.log(db)
         console.log(db[1].content)
-        onClick(db)
+        Theme(db)
+    })
+fetch("./contraint.json")
+    .then(function(data){
+        return data.json()
+    })
+    .then(function(data){
+        const db = data
+        console.log(db)
+        console.log(db[1].content)
+        Contrainte(db)
     })
 
 
@@ -14,16 +24,28 @@ fetch("./idea.json")
     Action onClick that'll search for a randomised number in the database, and return it with the elements of said item. 
     */
 
-const onClick = (db) => {
-    document.getElementById("generate").addEventListener("click", function(e){
+const Theme = (db) => {
+    document.getElementById("theme").addEventListener("click", function(e){
         e.preventDefault()
         const random = Math.floor(Math.random() * db.length)
-        const type = document.getElementById("type")
-        const content = document.getElementById("content")
-        const author = document.getElementById("author")
-        const number = document.getElementById("number")
+        const content = document.getElementById("theme-content")
+        const author = document.getElementById("theme-author")
+        const number = document.getElementById("theme-number")
         
-        type.textContent = `${db[random].type}`
+        content.textContent = `${db[random].content}`
+        author.textContent = `${db[random].author}`
+        number.textContent = `N° ${random + 1} / ${db.length}`
+    })
+}
+
+const Contrainte = (db) => {
+    document.getElementById("contrainte").addEventListener("click", function(e){
+        e.preventDefault()
+        const random = Math.floor(Math.random() * db.length)
+        const content = document.getElementById("contrainte-content")
+        const author = document.getElementById("contrainte-author")
+        const number = document.getElementById("contrainte-number")
+        
         content.textContent = `${db[random].content}`
         author.textContent = `${db[random].author}`
         number.textContent = `N° ${random + 1} / ${db.length}`
